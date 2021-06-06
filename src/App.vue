a2f7913aa692872deae2f2d27c03e77881fc5208
Aman Yerpude <aman.yerpude@codenicely.in>
2021-06-07 00:33:19 +0530

    WIP: stable ui first

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,62 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-main>
-      <router-view />
+     <v-parallax height="300" dark src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"></v-parallax>
+     <v-container>
+       <v-row class="justify-center" >
+         <v-col cols="10" md="6">
+           <v-card>
+         <v-list-item-group v-model="clickedTask" >
+        <v-list-item :key="task.id" v-for="task in taskList" inactive>
+           <template v-slot:default="{ active, }">
+            <v-list-item-action>
+              <v-checkbox
+                :input-value="active"
+                color="primary"
+              ></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>{{task.title}}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+          <v-divider inset></v-divider>
+        </v-list-item>
+      </v-list-item-group>
+
+       </v-card>
+         </v-col>
+       </v-row>
+     </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: "App",
 
   data: () => ({
-    //
+    taskList: [],
+    clickedTask: ''
   }),
+  methods: {
+    loadTaskList() {
+      // GET request using axios with set headers
+      const headers = { "Content-Type": "application/json" };
+      axios.get("http://generaltodoapi.pythonanywhere.com/api/task-list/", { headers })
+    .then(response => this.taskList = response.data)
+    .catch(error => {
+      this.errorMessage = error.message;
+      console.error("There was an error!", error);
+    });
+    }
+  },
+  created () {
+    axios.defaults.xsrfCookieName = 'csrftoken'
+    axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN'
+    this.loadTaskList()
+  }
 };
 </script>
